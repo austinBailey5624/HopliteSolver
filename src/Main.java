3b68d488aafb45bc27eec0f95c6b34157db85c96
Austin Bailey <a604b997@ku.edu>
2018-03-01 16:29:25 -0600

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/*
+ * Author: Austin Bailey (a604b997@gmail.com), github:austinBailey5624
+ * 
+ */
+public class Main 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!");
+	}
+}
